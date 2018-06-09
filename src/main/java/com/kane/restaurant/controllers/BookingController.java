38cf72f74f274edf76db21cefb76ac0e19aa5e01
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-06-09 20:51:44 +0100

    adds calendar with todays date preselected

--- src/main/java/com/kane/restaurant/controllers/BookingController.java
+++ src/main/java/com/kane/restaurant/controllers/BookingController.java
@@ -1,9 +1,14 @@
 package com.kane.restaurant.controllers;
 
+import com.kane.restaurant.db.DBHelper;
+import com.kane.restaurant.models.Customer;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.List;
 
 import static spark.Spark.get;
 
@@ -15,6 +20,13 @@ public class BookingController {
 
         get("/booking", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
+            GregorianCalendar today = new GregorianCalendar();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setCalendar(today);
+            List<Customer> allCustomers = DBHelper.getAllByAscending("name", Customer.class);
+
+            model.put("customers", allCustomers);
+            model.put("todaysDate", sdf.format(today.getTime()));
             model.put("template", "/templates/bookings/new.vtl");
             return new ModelAndView(model, "/templates/layout.vtl");
         }, velocityTemplateEngine);
